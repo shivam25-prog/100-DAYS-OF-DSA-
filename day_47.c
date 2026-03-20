c552491732d20ebf63f7b402d566d124fe1c9c38
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-03-20 22:19:28 +0530

    Add files via upload

---
+++ day_47.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to find height (in nodes)
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    /*
+            1
+           / \
+          2   3
+         /
+        4
+    */
+
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    printf("Height of tree: %d\n", height(root));
+
+    return 0;
+}
