f9c6f732e4cb02a13bd28d62a5c15965e966c403
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-03-22 10:06:16 +0530

    Add files via upload

---
+++ day_50.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to count leaf nodes
+int countLeaf(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    // If leaf node
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaf(root->left) + countLeaf(root->right);
+}
+
+int main() {
+    /*
+            1
+           / \
+          2   3
+         / \
+        4   5
+    */
+
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Leaf nodes count: %d\n", countLeaf(root));
+
+    return 0;
+}
