0e2f6f5c1b4a3133ee622101e31fab4e53f01cce
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-04-04 21:54:25 +0530

    Add files via upload

---
+++ day_63.c
@@ -0,0 +1,94 @@
+//Perform DFS starting from a given source vertex using recursion.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Graph structure
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+    int* visited;
+};
+
+// Create a new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Create a graph with n vertices
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = (struct Node**)malloc(vertices * sizeof(struct Node*));
+    graph->visited = (int*)malloc(vertices * sizeof(int));
+
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+    return graph;
+}
+
+// Add edge (undirected for simplicity)
+void addEdge(struct Graph* graph, int src, int dest) {
+    // src -> dest
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    // dest -> src
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+// Recursive DFS
+void DFS(struct Graph* graph, int vertex) {
+    graph->visited[vertex] = 1;
+    printf("%d ", vertex);
+
+    struct Node* temp = graph->adjLists[vertex];
+    while (temp) {
+        int adjVertex = temp->vertex;
+        if (!graph->visited[adjVertex]) {
+            DFS(graph, adjVertex);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    struct Graph* graph = createGraph(n);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v);
+    }
+
+    int source;
+    printf("Enter source vertex for DFS: ");
+    scanf("%d", &source);
+
+    printf("DFS Traversal starting from vertex %d:\n", source);
+    DFS(graph, source);
+
+    return 0;
+}
