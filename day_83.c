1254c0265ba7a200fa8fe5843098c2d3304eae29
shivam25-prog <Shivam.25572@stu.upes.ac.in>
2026-04-24 21:56:12 +0530

    Add files via upload

---
+++ day_83.c
@@ -0,0 +1,35 @@
+// Implement Selection Sort - Implement the algorithm.
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        
+        int minIndex = i;
+
+        // Find index of minimum element
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
